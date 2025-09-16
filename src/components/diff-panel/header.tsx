d2d444dc604c0829efa9fe299d41ea395124624d
babyjazz <bbos1@windowslive.com>
2025-09-16 18:13:17 +0700

    add pr title and styles

--- src/components/diff-panel/header.tsx
+++ src/components/diff-panel/header.tsx
@@ -4,10 +4,26 @@ import {
   EllipsisIcon,
   MessageSquareIcon,
 } from 'lucide-react'
+import { cn } from '@/utils/clsx'
 
-export default function Header() {
+interface HeaderProps {
+  isOpen: boolean
+  handleFold: (value: boolean) => void
+}
+
+export default function Header({ isOpen, handleFold }: HeaderProps) {
+  const handelComment = () => {
+    // eslint-disable-next-line
+    alert('No implement')
+  }
+
   return (
-    <div className="rounded-t-md text-xs items-center border-b-0 bg-secondary-bg border flex justify-between border-border-default p-2 text-text-primary -mb-0.5">
+    <div
+      className={cn(
+        'rounded-t-md text-xs items-center bg-secondary-bg border flex justify-between border-border-default p-2 text-text-primary -mb-0.5',
+        [isOpen ? 'border-b-0' : 'rounded-b-md'],
+      )}
+    >
       <div className="flex gap-2 items-center">
         <ChevronDownIcon className="w-3" />
         <span>src/components/some-file.tsx</span>
@@ -15,10 +31,15 @@ export default function Header() {
       </div>
       <div className="flex gap-2 items-center">
         <div className="flex gap-1">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            onChange={(e) => handleFold(e.target.checked)}
+          />
           <label>Viewed</label>
         </div>
-        <MessageSquareIcon className="w-3" />
+        <div onClick={handelComment} className="cursor-pointer">
+          <MessageSquareIcon className="w-3" />
+        </div>
         <EllipsisIcon className="w-3" />
       </div>
     </div>
--- src/components/diff-panel/index.tsx
+++ src/components/diff-panel/index.tsx
@@ -1,20 +1,33 @@
+import { useState } from 'react'
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from '@radix-ui/react-collapsible'
+import { cn } from '@/utils/clsx'
 import CodeView from './code-view'
 import Header from './header'
 
 export function DiffPanel() {
+  const [isOpen, setIsOpen] = useState(true)
+
+  const handleFold = (value: boolean) => {
+    setIsOpen(!value)
+  }
+
   return (
     <div className="rounded w-full">
-      <Collapsible>
+      <Collapsible onOpenChange={setIsOpen} open={isOpen}>
         <CollapsibleTrigger className="w-full">
-          <Header />
+          <Header isOpen={isOpen} handleFold={handleFold} />
         </CollapsibleTrigger>
 
-        <CollapsibleContent className="w-full bg-primary-bg rounded border-t-0 border border-border-default">
+        <CollapsibleContent
+          className={cn(
+            'w-full bg-primary-bg rounded border-t-0 border border-border-default',
+            [isOpen ? 'border-t-0' : 'border-t-0'],
+          )}
+        >
           <CodeView />
         </CollapsibleContent>
       </Collapsible>
